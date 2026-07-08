0c4937de32000fd198a8b008b5a6c6e1fdc2868c
John <4448701+freechie@users.noreply.github.com>
2026-07-08 00:23:31 -0400

    feat(ios): add SwiftUI app scaffold

---
+++ ios/Audio2Score/Audio2Score/Audio2ScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Audio2ScoreApp.swift
+//  Audio2Score
+//
+//  Created by richie on 7/8/26.
+//
+
+import SwiftUI
+
+@main
+struct Audio2ScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/Audio2Score/Audio2ScoreTests/Audio2ScoreTests.swift
@@ -0,0 +1,19 @@
+//
+//  Audio2ScoreTests.swift
+//  Audio2ScoreTests
+//
+//  Created by richie on 7/8/26.
+//
+
+import Testing
+@testable import Audio2Score
+
+struct Audio2ScoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
